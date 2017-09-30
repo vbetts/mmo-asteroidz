2c846c07753839efbf4e196c516c1b11d2abc3bb
Victoria Betts <victoriabetts@Perseus.local>
2017-09-30 17:06:53 -0400

    Refactor to store objects instead of JSON formatted arrays of data

--- application.py
+++ application.py
@@ -17,16 +17,35 @@ def index():
 
 @socketio.on('join', namespace='/test')
 def join_event(joinmsg):
-    current_sock = request.sid
     user = Player(request.sid)
     ship = Spaceship()
-    players[user.playerid] = {"shipid":ship.shipid, "colour":ship.colour, "x":ship.x, "y":ship.y, "v":ship.velocity}
-    
-    emit('joined', players)
+    user.shipid = ship.shipid
+
+    #store arrays of ship and player objects in memory indexed by respective IDs
+    ships[ship.shipid]=ship
+    players[user.playerid]=user
+
+    #Python objects are not translatable to JSON
+    #Build a JSON object containing ship data to return to the client
+    players_json = build_json()
+
+    emit('joined', players_json)
 
 @socketio.on('disconnect', namespace='/test')
 def leave_event():
+    #Remove players who have disconnected from the stored game data
+    player = players[request.sid]
     del players[request.sid]
+    del ships[player.shipid]
+
+def build_json():
+    #Python dictionaries are essentially the same format as JSON
+    ret = {}
+    for player in players:
+        ship = ships[players[player].shipid]
+        #index the data by playerid
+        ret[player] = {"shipid":ship.shipid, "colour":ship.colour, "x":ship.x, "y":ship.y, "v":ship.velocity}
+    return ret
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
--- classes/Player.py
+++ classes/Player.py
@@ -1,4 +1,5 @@
 class Player:
     def __init__(self, playerid):
         self.playerid = playerid
+        self.shipid = ""
 
