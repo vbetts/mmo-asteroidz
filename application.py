3595cbff8476995119ac4aef944bdf9278cbee39
Victoria Betts <victoriabetts@Perseus.local>
2017-09-30 22:14:44 -0400

    Added asynchronous game updates

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from threading import Lock
 from random import randint
 from classes.Player import Player
 from classes.Spaceship import Spaceship
@@ -7,6 +8,8 @@ from classes.Spaceship import Spaceship
 app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
+thread = None
+thread_lock = Lock()
 
 all_players = {}
 all_ships = {}
@@ -15,8 +18,11 @@ all_ships = {}
 def index():
         return render_template('index.html')
 
+
 @socketio.on('join', namespace='/test')
 def join_event(joinmsg):
+    global all_players
+    global all_ships
     user = Player(request.sid)
     ship = Spaceship()
     user.shipid = ship.shipid
@@ -28,37 +34,61 @@ def join_event(joinmsg):
     #Python objects are not translatable to JSON
     #Build a JSON object containing ship data to return to the client
     players_json = build_json()
-
+    init_game()
     emit('draw', players_json)
 
-
+#move_ship is triggered when the user presses an arrow key
+#This function sets the direction of movement
 @socketio.on('move_ship', namespace='/test')
 def move_event(direction):
+    global all_players
+    global all_ships
     player = all_players[request.sid]
     ship = all_ships[player.shipid]
-    ship = ship.move(ship, direction)
+    ship.set_direction(direction)
     
-    all_ships[player.shipid] = ship
+    game_data = build_json()
 
-    ret = build_json()
+    emit('draw', game_data)
 
-    emit('draw', ret)
 
-@socketio.on('disconnect', namespace='/test')
-def leave_event():
-    #Remove players who have disconnected from the stored game data
-    player = all_players[request.sid]
-    del all_players[request.sid]
-    del all_ships[player.shipid]
+#Start the background thread to 'animate' the game objects
+def init_game():
+    global thread
+    with thread_lock:
+            if thread is None:
+                thread = socketio.start_background_task(target=update_game)
 
+#Re-draw the canvas 30x a second
+def update_game():
+    global all_ships
+    while True:
+        socketio.sleep(1.0/30.0)
+        for ship in all_ships:
+            all_ships[ship].move()
+        game_data = build_json()
+        socketio.emit('draw', game_data, namespace='/test')
+
+#Builds data in format that the client side can understand
 def build_json():
+    global all_players
+    global all_ships
     #Python dictionaries are essentially the same format as JSON
     ret = {}
     for player in all_players:
         ship = all_ships[all_players[player].shipid]
         #index the data by playerid
-        ret[player] = {"shipid":ship.shipid, "colour":ship.colour, "x":ship.x, "y":ship.y, "v":ship.velocity}
+        ret[player] = {'shipid':ship.shipid, 'colour':ship.colour, 'x':ship.x, 'y':ship.y, 'v':ship.velocity, 'rotation':ship.rotation}
     return ret
+
+@socketio.on('disconnect', namespace='/test')
+def leave_event():
+    global all_players
+    global all_ships
+    #Remove players who have disconnected from the stored game data
+    player = all_players[request.sid]
+    del all_players[request.sid]
+    del all_ships[player.shipid]
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
--- classes/Game_Object.py
+++ classes/Game_Object.py
@@ -7,35 +7,38 @@ class Game_Object:
     def __init__(self):
         self.direction = ""
         self.velocity = 0
+        self.rotation = 0
         self.x = random.randint(0, 600)
         self.y = random.randint(0, 400)
 
-    def move(self, obj, direction):
-        if obj.direction != "" and direction == OPPOSITE_DIRECTION[obj.direction]:
-            obj.velocity = 0
-        else: 
-            obj.velocity = 1
+    def set_direction(self, direction):
+        if self.direction != "" and direction == OPPOSITE_DIRECTION[self.direction]:
+            self.velocity = 0
+        else:
+            self.velocity = 1
+            #the rotation here can be 90 or 270
 
-        obj.direction = direction
+        self.direction = direction
+    
+    #Moves the ship continuously once the user chooses a direction
+    def move(self):
+        if self.direction == "left":
+            self.x = self.x-self.velocity
+        elif self.direction == "right":
+            self.x = self.x+self.velocity
+        elif self.direction == "up":
+            self.y = self.y-self.velocity
+        elif self.direction == "down":
+            self.y = self.y+self.velocity
 
-        if direction == "left":
-            obj.x = obj.x-obj.velocity
-        elif direction == "right":
-            obj.x = obj.x+obj.velocity
-        elif direction == "up":
-            obj.y = obj.y-obj.velocity
-        elif direction == "down":
-            obj.y = obj.y+obj.velocity
-
-        if obj.x > MAX_WIDTH:
-            obj.x = 0
-        elif obj.x < 0:
-            obj.x = MAX_WIDTH
+        if self.x > MAX_WIDTH:
+            self.x = 0
+        elif self.x < 0:
+            self.x = MAX_WIDTH
         
-        if obj.y > MAX_HEIGHT:
-            obj.y = 0
-        elif obj.y < 0:
-            obj.y = MAX_HEIGHT
+        if self.y > MAX_HEIGHT:
+            self.y = 0
+        elif self.y < 0:
+            self.y = MAX_HEIGHT
 
-        return obj
 
